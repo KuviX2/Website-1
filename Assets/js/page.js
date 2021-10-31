aeb634d1a7554ba632d088141b418c02732f257c
Taco_Marco <tacoomarcoo@gmail.com>
2021-10-31 15:11:19 +1100

    Added dark theme and Assets folder

---
+++ Assets/js/page.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    const theme = document.querySelector("#theme-link");
+
+    if (theme.getAttribute("href") == "Assets/css/light-mode.css") {
+        theme.href = "Assets/css/dark-mode.css";
+    }
+
+    else {
+        theme.href = "Assets/css/light-mode.css";
+    }
+}
