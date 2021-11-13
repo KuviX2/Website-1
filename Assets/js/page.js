2f1c35147e0c2dce2cb9c82fa94e51fde36fd659
TacoMarco <tacoomarcoo@gmail.com>
2021-11-13 11:56:04 +1100

    Made new page, moved old index

--- Assets/js/page.js
+++ Assets/js/page.js
@@ -1,11 +1,11 @@
 function myFunction() {
     const theme = document.querySelector("#theme-link");
 
-    if (theme.getAttribute("href") == "Assets/css/light-mode.css") {
-        theme.href = "Assets/css/dark-mode.css";
+    if (theme.getAttribute("href") == "../Assets/css/light-mode.css") {
+        theme.href = "../Assets/css/dark-mode.css";
     }
 
     else {
-        theme.href = "Assets/css/light-mode.css";
+        theme.href = "../Assets/css/light-mode.css";
     }
 }
