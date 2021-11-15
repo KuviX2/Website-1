b412b6e266b7c32d552bbb8b9404a5754304b1d2
TacoMarco <tacoomarcoo@gmail.com>
2021-11-15 21:03:37 +1100

    Added redirect to mobile page (if on mobile)

---
+++ Assets/js/detect-mobile.js
@@ -0,0 +1,22 @@
+var userAgent = navigator.userAgent.toLowerCase(), width = screen.availWidth, height = screen.availHeight, userIsOnMobileDevice = checkIfUserIsOnMobileDevice(userAgent);
+
+if(userIsOnMobileDevice) {
+    window.location.href = "mobile"
+} 
+
+function checkIfUserIsOnMobileDevice($userAgent) {
+    if($userAgent.includes("mobi") || $userAgent.includes("tablet")) {
+        return true;
+    }
+    if($userAgent.includes("android")) {
+        if(height > width && width < 800) {
+            // Screen is higher than it"s wide, so we have portrait mode
+            return true;
+        }
+        if(width > height && height < 800) {
+            // Screen is wider than it"s high, so we have landscape mode
+            return true;
+        }
+    }
+   return false;
+}
